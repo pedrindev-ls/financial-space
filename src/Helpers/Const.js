e3d3906356602f36fb53fddcb0d4c24f57ff9ec0
Pedrin <pedro515vieira@gmail.com>
2022-06-01 14:24:47 -0300

    Feat: Adicionando os links de Requisição

---
+++ src/Helpers/Const.js
@@ -0,0 +1,6 @@
+export const currencies = 'https://api.hgbrasil.com/finance';
+export const stocks = 'https://api.hgbrasil.com/finance/stock_price?key=3e768391&symbol="stock"';
+// https://api.hgbrasil.com/finance/stock_price?key=3e768391&symbol=get-low
+// https://api.hgbrasil.com/finance/stock_price?key=3e768391&symbol=get-high
+// https://api.hgbrasil.com/finance/quotations?key=3e768391 cotações das moedas
+// https://api.hgbrasil.com/finance/taxes?key=3e768391 taxas de rnda fixa
